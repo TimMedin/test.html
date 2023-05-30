f943fb11d017fb4fd0c400e1ba06ac0e40ae4a8d
TimMedin <timmedin180@gmail.com>
2023-05-30 15:36:56 +0200

    update

--- main.js
+++ main.js
@@ -6,102 +6,22 @@ var questions = {
   question1: {
     question: "Що таке HTML?",
     answers: {
-      А: "А: Hyperlinks and Text Markup Language",
-      Б: "Б: Hyper Text Markup Language",
-      В: "В' Hyperlinking and Text Markup Language",
-      Г: "Г: Home Tool Markup Language"
+      А: "Hyperlinks and Text Markup Language",
+      Б: "Hyper Text Markup Language",
+      В: "Hyperlinking and Text Markup Language",
+      Г: "Home Tool Markup Language"
     },
     correctAnswer: "Б"
   },
   question2: {
-    question: "Яким тегом позначається посилання?",
+    question: "Питання 2",
     answers: {
-      А: "А: &lt;a&gt;",
-      Б: "Б: &lt;div&gt;",
-      В: "В: &lt;link&gt;",
-      Г: "Г: &lt;br&gt;"
+      А: "Відповідь А",
+      Б: "Відповідь Б",
+      В: "Відповідь В",
+      Г: "Відповідь Г"
     },
     correctAnswer: "А"
-  },
-  question3: {
-    question: "Яким тегом позначається головний заголовок на сайті?",
-    answers: {
-      А: "А: &lt;title&gt;",
-      Б: "Б: &lt;p&gt;",
-      В: "В: &lt;a&gt;",
-      Г: "Г: &lt;h1&gt;"
-    },
-    correctAnswer: "Г"
-  },
-  question4: {
-    question: "В якому тегі міститься інформація для браузера?",
-    answers: {
-      А: "А: &lt;html&gt;",
-      Б: "Б: &lt;head&gt;",
-      В: "В: &lt;body&gt;",
-      Г: "Г: &lt;header&gt;"
-    },
-    correctAnswer: "Б"
-  },
-  question5: {
-    question: "Що таке CSS?",
-    answers: {
-      А: "А: Це файл, який відповідає за розмітку сайту.",
-      Б: "Б: Це службова інформація для браузера;",
-      В: "В: CSS - стилі для сайту, які дозволяють позиціонувати елементи, змінювати їх колір і т.д",
-      Г: "Г: Жодна відповідь неправильна."
-    },
-    correctAnswer: "В"
-  },
-  question6: {
-    question: "Який файл є головним для веб-сторінки?",
-    answers: {
-      А: "А: text.html",
-      Б: "Б: script.html",
-      В: "В: main.html",
-      Г: "Г: index.html"
-    },
-    correctAnswer: "Г"
-  },
-  question7: {
-    question: "За допомогою якого тегу можна додати кнопку на сайт?",
-    answers: {
-      А: "А: &lt;a&gt;",
-      Б: "Б: &lt;input&gt;",
-      В: "В: &lt;button&gt;",
-      Г: "Г: &lt;link&gt;"
-    },
-    correctAnswer: "В"
-  },
-  question8: {
-    question: "Що означає тег  &lt;p&gt",
-    answers: {
-      А: "А: Головний заголовок на сайті.",
-      Б: "Б: Текст, абзац",
-      В: "В: Поле для введеня",
-      Г: "Г: Жодна відповідь неправильна"
-    },
-    correctAnswer: "Б"
-  },
-  question9: {
-    question: "Який атрибут потрібен для коректної роботи тегу  &lt;img&gt",
-    answers: {
-      А: "А: Атрибути: href, alt",
-      Б: "Б: Атрибути: src, href",
-      В: "В: Атрибути: alt, src",
-      Г: "Г: Атрибути: href, target"
-    },
-    correctAnswer: "В"
-  },
-  question10: {
-    question: "В якому тегі записується зміст сайту?",
-    answers: {
-      А: "А: &lt;head&gt",
-      Б: "Б: &lt;title&gt",
-      В: "В: &lt;body&gt",
-      Г: "Г  &lt;html&gt"
-    },
-    correctAnswer: "В"
   },
   // Додайте решту запитань тут
 };
