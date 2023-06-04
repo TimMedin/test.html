666fee01526a3f561fc767ed3cf5468baf8df137
TimMedin <timmedin180@gmail.com>
2023-06-04 20:35:50 +0200

    updates

--- main.js
+++ main.js
@@ -1,268 +1,101 @@
-var currentQuestion = 1;
-var score = 0;
-var userAnswers = {};
+const questions = [
+  {
+    question: "Перше запитання?",
+    answers: ["А", "Б", "В", "Г"],
+    correctAnswer: 1
+  },
+  {
+    question: "Друге запитання?",
+    answers: ["А", "Б", "В", "Г"],
+    correctAnswer: 0
+  },
+  {
+    question: "Третє запитання?",
+    answers: ["А", "Б", "В", "Г"],
+    correctAnswer: 0
+  },
+  {
+    question: "Четверте запитання?",
+    answers: ["А", "Б", "В", "Г"],
+    correctAnswer: 2
+  },
+  {
+    question: "П'яте запитання?",
+    answers: ["А", "Б", "В", "Г"],
+    correctAnswer: 0
+  },
+  {
+    question: "Шосте запитання?",
+    answers: ["А", "Б", "В", "Г"],
+    correctAnswer: 3
+  },
+  {
+    question: "Сьоме запитання?",
+    answers: ["А", "Б", "В", "Г"],
+    correctAnswer: 1
+  },
+  {
+    question: "Восьме запитання?",
+    answers: ["А", "Б", "В", "Г"],
+    correctAnswer: 1
+  },
+  {
+    question: "Дев'яте запитання?",
+    answers: ["А", "Б", "В", "Г"],
+    correctAnswer: 3
+  },
+  {
+    question: "Десяте запитання?",
+    answers: ["А", "Б", "В", "Г"],
+    correctAnswer: 0
+  },
+  {
+    question: "Одинадцяте запитання?",
+    answers: ["А", "Б", "В", "Г"],
+    correctAnswer: 2
+  },
+  {
+    question: "Дванадцяте запитання?",
+    answers: ["А", "Б", "В", "Г"],
+    correctAnswer: 3
+  }
+];
 
-var questions = {
-  question1: {
-    question: "Що таке HTML?",
-    answers: {
-      А: "А: Hyperlinks and Text Markup Language",
-      Б: "Б: Hyper Text Markup Language",
-      В: "В' Hyperlinking and Text Markup Language",
-      Г: "Г: Home Tool Markup Language"
-    },
-    correctAnswer: "Б"
-  },
-  question2: {
-    question: "Яким тегом позначається посилання?",
-    answers: {
-      А: "А: &lt;a&gt;",
-      Б: "Б: &lt;div&gt;",
-      В: "В: &lt;link&gt;",
-      Г: "Г: &lt;br&gt;"
-    },
-    correctAnswer: "А"
-  },
-  question3: {
-    question: "Яким тегом позначається головний заголовок на сайті?",
-    answers: {
-      А: "А: &lt;title&gt;",
-      Б: "Б: &lt;p&gt;",
-      В: "В: &lt;a&gt;",
-      Г: "Г: &lt;h1&gt;"
-    },
-    correctAnswer: "Г"
-  },
-  question4: {
-    question: "В якому тегі міститься інформація для браузера?",
-    answers: {
-      А: "А: &lt;html&gt;",
-      Б: "Б: &lt;head&gt;",
-      В: "В: &lt;body&gt;",
-      Г: "Г: &lt;header&gt;"
-    },
-    correctAnswer: "Б"
-  },
-  question5: {
-    question: "Що таке CSS?",
-    answers: {
-      А: "А: Це файл, який відповідає за розмітку сайту.",
-      Б: "Б: Це службова інформація для браузера;",
-      В: "В: CSS - стилі для сайту, які дозволяють позиціонувати елементи, змінювати їх колір і т.д",
-      Г: "Г: Жодна відповідь неправильна."
-    },
-    correctAnswer: "В"
-  },
-  question6: {
-    question: "Який файл є головним для веб-сторінки?",
-    answers: {
-      А: "А: text.html",
-      Б: "Б: script.html",
-      В: "В: main.html",
-      Г: "Г: index.html"
-    },
-    correctAnswer: "Г"
-  },
-  question7: {
-    question: "За допомогою якого тегу можна додати кнопку на сайт?",
-    answers: {
-      А: "А: &lt;a&gt;",
-      Б: "Б: &lt;input&gt;",
-      В: "В: &lt;button&gt;",
-      Г: "Г: &lt;link&gt;"
-    },
-    correctAnswer: "В"
-  },
-  question8: {
-    question: "Що означає тег  &lt;p&gt",
-    answers: {
-      А: "А: Головний заголовок на сайті.",
-      Б: "Б: Текст, абзац",
-      В: "В: Поле для введеня",
-      Г: "Г: Жодна відповідь неправильна"
-    },
-    correctAnswer: "Б"
-  },
-  question9: {
-    question: "Який атрибут потрібен для коректної роботи тегу  &lt;img&gt",
-    answers: {
-      А: "А: Атрибути: href, alt",
-      Б: "Б: Атрибути: src, href",
-      В: "В: Атрибути: alt, src",
-      Г: "Г: Атрибути: href, target"
-    },
-    correctAnswer: "В"
-  },
-  question10: {
-    question: "В якому тегі записується зміст сайту?",
-    answers: {
-      А: "А: &lt;head&gt",
-      Б: "Б: &lt;title&gt",
-      В: "В: &lt;body&gt",
-      Г: "Г  &lt;html&gt"
-    },
-    correctAnswer: "В"
-  },
-};
+let currentQuestion = 0;
+let score = 0;
+let quizCompleted = false;
 
 function showQuestion() {
-  var questionContainer = document.getElementById("questionContainer");
-  var currentQuestionObj = questions["question" + currentQuestion];
+  const quizContainer = document.getElementById("quizContainer");
+  quizContainer.innerHTML = "";
 
-  questionContainer.innerHTML = "";
-  questionContainer.innerHTML += "<h2>Питання " + currentQuestion + ":</h2>";
-  questionContainer.innerHTML += "<p>" + currentQuestionObj.question + "</p>";
+  const questionContainer = document.createElement("div");
+  questionContainer.id = "questionContainer";
+  questionContainer.innerHTML = `
+    <h2>${questions[currentQuestion].question}</h2>
+    <ul>
+      ${questions[currentQuestion].answers
+        .map(
+          (answer, index) =>
+            `<li>
+              <input type="radio" id="answer${index}" name="answer" value="${index}">
+              <label for="answer${index}">${answer}</label>
+            </li>`
+        )
+        .join("")}
+    </ul>
+  `;
 
-  for (var answer in currentQuestionObj.answers) {
-    var input =
-      '<input type="radio" name="question' +
-      currentQuestion +
-      '" value="' +
-      answer +
-      '"> ' +
-      currentQuestionObj.answers[answer] +
-      "<br>";
+  quizContainer.appendChild(questionContainer);
 
-    if (userAnswers["question" + currentQuestion] === answer) {
-      input = input.replace("radio", "radio" + currentQuestion).replace("value=", "checked value=");
-    }
-
-    questionContainer.innerHTML += input;
-  }
-
-  document.getElementById("nextButton").style.display = "block";
-  document.getElementById("submitButton").style.display = "none";
+  const nextButton = document.getElementById("nextButton");
+  nextButton.style.display = "block";
+  nextButton.style.animation = "fade-in 0.5s forwards";
 }
 
 function checkAnswer() {
-  var selectedAnswer = document.querySelector(
-    'input[name="question' + currentQuestion + '"]:checked'
-  ).value;
-
-  userAnswers["question" + currentQuestion] = selectedAnswer;
-
-  var currentQuestionObj = questions["question" + currentQuestion];
-
-  if (selectedAnswer === currentQuestionObj.correctAnswer) {
-    score++;
-  }
-
-  currentQuestion++;
-
-  if (currentQuestion > Object.keys(questions).length) {
-    finishQuiz();
-  } else {
-    showQuestion();
-  }
-}
-
-function finishQuiz() {
-  var resultContainer = document.getElementById("resultContainer");
-  var resultHTML = "<h2 class='h2-heading'>Результат</h2>";
-  var percentage = (score / Object.keys(questions).length) * 100;
-
-  resultHTML +=
-    "<p id='score'>Ваша оцінка: " +
-    score +
-    "/" +
-    Object.keys(questions).length +
-    " (" +
-    percentage +
-    "%)</p>";
-
-  if (percentage === 100) {
-    resultHTML += "<p>Вітаємо! Ви правильно відповіли на всі запитання!</p>";
-  } else {
-    resultHTML +=
-      "<p>Ваші помилки та правильні відповіді:</p><ul>";
-
-    for (var i = 1; i <= Object.keys(questions).length; i++) {
-      var questionObj = questions["question" + i];
-      var userAnswer = userAnswers["question" + i];
-      var isCorrect = userAnswer === questionObj.correctAnswer;
-
-      resultHTML += "<li><strong>Питання " + i + ":</strong> " + questionObj.question + "<br>";
-      resultHTML += "Ваша відповідь: " + questionObj.answers[userAnswer] + "<br>";
-
-      if (isCorrect) {
-        resultHTML += "<span class='correct'>Правильно!</span><br>";
-      } else {
-        resultHTML +=
-          "<span class='incorrect'>Неправильно. Правильна відповідь: " +
-          questionObj.answers[questionObj.correctAnswer] +
-          "</span><br>";
-      }
-
-      resultHTML += "</li>";
-    }
-
-    resultHTML += "</ul>";
-  }
-
-  resultContainer.innerHTML = resultHTML;
-
-  document.getElementById("quizContainer").style.display = "none";
-  resultContainer.style.display = "block";
-
-  // Зберігаємо результати тесту в локальному сховищі
-  localStorage.setItem("quizCompleted", "true");
-  localStorage.setItem("quizScore", score);
-}
-
-// Перевірка, чи користувач вже пройшов тест
-if (localStorage.getItem("quizCompleted")) {
-  var score = localStorage.getItem("quizScore");
-
-  var resultContainer = document.getElementById("resultContainer");
-  var resultHTML = "<h2 class='h2-heading'>Результат</h2>";
-  var percentage = (score / Object.keys(questions).length) * 100;
-
-  resultHTML +=
-    "<p id='score'>Ваша оцінка: " +
-    score +
-    "/" +
-    Object.keys(questions).length +
-    " (" +
-    percentage +
-    "%)</p>";
-
-  if (percentage === 100) {
-    resultHTML += "<p>Вітаємо! Ви правильно відповіли на всі запитання!</p>";
-  } else {
-    resultHTML +=
-      "<p>Ваші помилки та правильні відповіді:</p><ul>";
-
-    for (var i = 1; i <= Object.keys(questions).length; i++) {
-      var questionObj = questions["question" + i];
-      var userAnswer = userAnswers["question" + i];
-      var isCorrect = userAnswer === questionObj.correctAnswer;
-
-      resultHTML += "<li><strong>Питання " + i + ":</strong> " + questionObj.question + "<br>";
-      resultHTML += "Ваша відповідь: " + questionObj.answers[userAnswer] + "<br>";
-
-      if (isCorrect) {
-        resultHTML += "<span class='correct'>Правильно!</span><br>";
-      } else {
-        resultHTML +=
-          "<span class='incorrect'>Неправильно. Правильна відповідь: " +
-          questionObj.answers[questionObj.correctAnswer] +
-          "</span><br>";
-      }
-
-      resultHTML += "</li>";
-    }
-
-    resultHTML += "</ul>";
-  }
-
-  resultContainer.innerHTML = resultHTML;
-
-  document.getElementById("quizContainer").style.display = "none";
-  resultContainer.style.display = "block";
-} else {
-  function checkAnswer() {
   const selectedAnswer = document.querySelector("input[name='answer']:checked");
-  if (!selectedAnswer) return;
+  if (!selectedAnswer || quizCompleted) return;
 
   const answerIndex = parseInt(selectedAnswer.value);
   const correctAnswerIndex = questions[currentQuestion].correctAnswer;
@@ -283,8 +116,6 @@ if (localStorage.getItem("quizCompleted")) {
     submitButton.style.display = "block";
     submitButton.style.animation = "fade-in 0.5s forwards";
   }
-
-  currentQuestion++;
 }
 
 function nextQuestion() {
@@ -295,40 +126,63 @@ function nextQuestion() {
     questionContainer.innerHTML = "";
     questionContainer.style.animation = "";
 
+    currentQuestion++;
     if (currentQuestion < questions.length) {
       showQuestion();
+    } else {
+      showResult();
     }
   }, 500);
 }
 
-function finishQuiz() {
+function showResult() {
   const quizContainer = document.getElementById("quizContainer");
-  quizContainer.style.animation = "slide-out 0.5s forwards";
+  quizContainer.innerHTML = "";
 
-  const resultContainer = document.getElementById("resultContainer");
+  const resultContainer = document.createElement("div");
+  resultContainer.id = "resultContainer";
   resultContainer.style.display = "block";
+  resultContainer.style.animation = "fade-in 0.5s forwards";
 
-  setTimeout(() => {
-    quizContainer.style.display = "none";
+  const resultMessage =
+    score === questions.length
+      ? "Вітаємо! Ви отримали максимальний бал!"
+      : `Ви отримали ${score} з ${questions.length} балів.`;
 
-    const resultMessage = score === questions.length ? "Вітаємо! Ви отримали максимальний бал!" : `Ви отримали ${score} з ${questions.length} балів.`;
-    const resultHTML = `
-      <h2>Результати</h2>
-      <p>${resultMessage}</p>
-      <h3>Робота над помилками:</h3>
-      <ul>
-        ${questions.map((question, index) => {
-          const answerIndex = parseInt(document.querySelector(`input[name='answer${index}']:checked`)?.value);
-          const correctAnswerIndex = question.correctAnswer;
-          const isCorrect = answerIndex === correctAnswerIndex;
-          const className = isCorrect ? "correct" : "incorrect";
-          return `<li class="${className}">${question.answers[correctAnswerIndex]}</li>`;
-        }).join("")}
-      </ul>
-    `;
-    resultContainer.innerHTML = resultHTML;
-    resultContainer.style.animation = "fade-in 0.5s forwards";
-  }, 500);
+  const resultHTML = `
+    <h2>Результати</h2>
+    <p>${resultMessage}</p>
+    <h3>Робота над помилками:</h3>
+    <ul>
+      ${questions
+        .map(
+          (question, index) =>
+            `<li class="${index === currentQuestion ? "current" : ""}">
+              ${question.answers[question.correctAnswer]}
+            </li>`
+        )
+        .join("")}
+    </ul>
+  `;
+
+  resultContainer.innerHTML = resultHTML;
+  quizContainer.appendChild(resultContainer);
+
+  quizCompleted = true;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const nextButton = document.getElementById("nextButton");
+  nextButton.addEventListener("click", function () {
+    checkAnswer();
+    nextQuestion();
+  });
+
+  const submitButton = document.getElementById("submitButton");
+  submitButton.addEventListener("click", function () {
+    checkAnswer();
+    showResult();
+  });
+
   showQuestion();
-}
+});
